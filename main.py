a2fe3cf9abfae9ff1ca0e99905c7eb297324b845
Emil Kenguerli <emil.kenguerli@gmail.com>
2021-11-07 00:27:31 +0200

    Feat: Implemented multiprocessing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import multiprocessing as mp
 import pandas as pd
 from pandas.errors import ParserError
 from watchdog.observers import Observer
@@ -6,8 +7,14 @@ from watchdog.events import PatternMatchingEventHandler
 
 def on_created(event):
     try:
+        pool.apply_async(createCSV, (event.src_path,))
+    except Exception as e:
+        print(e)
+
+def createCSV(src_path):
+    try:
         old_data= pd.read_csv(
-            event.src_path,
+            src_path,
             sep = '\t',
             encoding='UTF-16 LE',
             thousands=',',
@@ -35,8 +42,8 @@ def on_created(event):
     except ParserError:
         print("Your data contained rows that could not be parsed.")
 
-
 if __name__ == "__main__":
+    pool = mp.Pool(processes=mp.cpu_count())
     patterns = ["*.csv"]
     ignore_patterns = None
     ignore_directories = False
@@ -56,4 +63,6 @@ if __name__ == "__main__":
             time.sleep(1)
     except KeyboardInterrupt:
         my_observer.stop()
-        my_observer.join()
+    my_observer.join()
+    pool.close()
+    pool.join()
